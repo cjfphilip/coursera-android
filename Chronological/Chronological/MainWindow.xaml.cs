6da7819d66e0b4c91bdc448b447007c95b8745c9
Charles <charles@charlesphilip.com>
2015-06-20 20:48:50 +0100

    Working

--- Chronological/Chronological/MainWindow.xaml.cs
+++ Chronological/Chronological/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Windows;
 using System.Windows.Documents;
+using System.Linq;
 
 using Path = System.IO.Path;
 
@@ -13,90 +14,157 @@ namespace Chronological
 	/// </summary>
 	public partial class MainWindow : Window
 	{
-		const string path = @"C:\Users\Charles\Documents\Android\Source\coursera-android\Chronological\Chrono";
+        const string path = @"C:\Users\C3P4J\Documents\Android\Source\coursera-android\Chronological\Chrono";
 		List<MatchingLineToMp3> results = new List<MatchingLineToMp3>();
+        List<string> files = new List<string>();
+        int totalMp3s;
 
 		public MainWindow()
 		{
 			InitializeComponent();
+            ResultsListBox.ItemsSource = results;
+            FileSystemWatcher fileSystemWatcher = new FileSystemWatcher(path);
+            fileSystemWatcher.Changed += fileSystemWatcher_Changed;
 			Refresh();
+            //fileSystemWatcher.EnableRaisingEvents = true;
 		}
 
+        void fileSystemWatcher_Changed(object sender, FileSystemEventArgs e)
+        {
+            Refresh();
+        }
+
+        private Object refreshing = new Object();
 		void Refresh()
 		{
-			try
-			{
-				List<String> listOfItems = new List<string>();
-				string fileToParse = Path.Combine(path, "order.txt");
-				using (StreamReader sr = new StreamReader(fileToParse))
-				{
-					while (sr.Peek() >= 0)
-					{
-						String line = sr.ReadLine();
-						if (!string.IsNullOrWhiteSpace(line))
-						{
-							string message;
-							List<string> mp3S = FindMatchingMp3S(line, out message);
-							results.Add(new MatchingLineToMp3(line, mp3S, message));
-						}
-					}
-				}
-				ResultsListBox.ItemsSource = results;
-			}
-			catch (Exception e)
-			{
-				Console.WriteLine(e.Message);
-			}
+            lock (refreshing)
+            {
+                results = new List<MatchingLineToMp3>();
+                try
+                {
+                    files = Directory.GetFiles(Path.Combine(path, "books"), "*.mp3", SearchOption.AllDirectories).ToList();
+                    totalMp3s = files.Count;
+                    List<String> listOfItems = new List<string>();
+                    string fileToParse = Path.Combine(path, "order.txt");
+                    using (StreamReader sr = new StreamReader(fileToParse))
+                    {
+                        while (sr.Peek() >= 0)
+                        {
+                            String line = sr.ReadLine().Trim();
+                            if (!string.IsNullOrWhiteSpace(line))
+                            {
+                                FindMatchingMp3S(line);
+                            }
+                        }
+                    }
+
+                    foreach (string file in files)
+                    {
+                        results.Add(new MatchingLineToMp3(file, "[unknown]", 0, 0, new List<string>(), "not found in playlist"));
+                    }
+
+                    ResultsListBox.ItemsSource = results;
+                    int totalFound = results.SelectMany(l => l.mp3S).Count();
+                    btnRefresh.Content = String.Format("Copy Files - Found {0} out of {1}", totalFound, totalMp3s);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
 		}
 
-		List<string> FindMatchingMp3S(string line, out string message)
+		void FindMatchingMp3S(string line)
 		{
-			message = "no chapters found";
+            string message = "no chapters found";
 			List<string> result = new List<string>();
-			int splitPos = line.IndexOf(" ");
+			int splitPos = line.LastIndexOf(" ");
 			if (splitPos > -1)
 			{
 				try
 				{
 					string bookToFind = line.Substring(0, splitPos);
-					string chapterNumbers = line.Substring(splitPos);
+					string chapterNumbers = line.Substring(splitPos+1);
 					int hyphenPos = chapterNumbers.IndexOf("-");
 					int chapterToStartWith = int.Parse(hyphenPos > -1 ? chapterNumbers.Substring(0, hyphenPos) : chapterNumbers);
 					int chapterToEndWith = hyphenPos > -1 ? int.Parse(chapterNumbers.Substring(hyphenPos + 1)) : chapterToStartWith;
+                    int totalNeeded = chapterToEndWith - chapterToStartWith + 1;
+                    int start = chapterToStartWith;
 
-					string[] files = Directory.GetFiles(Path.Combine(path, "books"), "*.mp3", SearchOption.AllDirectories);
-					foreach (string file in files)
-					{
+					for (int i = 0; i < files.Count; i++)
+			        {
+			            var file = files[i];
 						string fileName = Path.GetFileName(file);
 
 						if (fileName != null && fileName.Substring(3).StartsWith(bookToFind))
 						{
-							for (int chapter = chapterToStartWith; chapter <= chapterToEndWith; chapter++)
-							{
-								string chapterToLookFor = String.Format("Chapter {0:D2}.mp3", chapter);
-								if (fileName.Contains(chapterToLookFor))
-								{
-									result.Add(fileName);
-									break;
-								}
-							}
+                            if (chapterToStartWith <= chapterToEndWith)
+                            {
+                                for (int chapter = chapterToStartWith; chapter <= chapterToEndWith; chapter++)
+                                {
+                                    string chapterToLookFor = String.Format("Chapter {0:D2}.mp3", chapter);
+                                    if (fileName.Contains(chapterToLookFor))
+                                    {
+                                        result.Add(file);
+                                        files.Remove(file);
+                                        i--;
+                                        chapterToStartWith += 1;
+                                        break;
+                                    }
+                                }
+                            }
+                            
+                            if (chapterToStartWith > chapterToEndWith)
+                            {
+                                break;
+                            }
 						}
 					}
 
-					int totalNeeded = chapterToEndWith - chapterToStartWith + 1;
-					message = result.Count == totalNeeded ? "success" : String.Format("{0} mp3s found, but there needs to be {1}", result.Count, totalNeeded);
+                    if (totalNeeded > 0)
+                    {
+                        message = result.Count == totalNeeded ? "success" : String.Format("{0} mp3s found, but there needs to be {1}", result.Count, totalNeeded);
+                        results.Add(new MatchingLineToMp3(line, bookToFind, start, chapterToEndWith, result, message));
+                    }
+                    else
+                    {
+                        message = "no chapters found";
+                        results.Add(new MatchingLineToMp3(line, bookToFind, chapterToStartWith, chapterToEndWith, result, message));
+                    }
 				}
 				catch (Exception ex)
 				{
 					message = ex.Message;
+                    results.Add(new MatchingLineToMp3(line, "???", 0, 0, result, message));
 				}
+                
 			}
-			return result;
+            else
+            {
+                results.Add(new MatchingLineToMp3(line, "no book can be found", 0, 0, result, message));
+
+            }
 		}
+
+        void CopyMp3s()
+        {
+            int i = 1;
+            foreach(MatchingLineToMp3 result in results)
+            {
+                string chapters = (result.start == result.end) ? result.start.ToString() : (result.start + "-" + result.end);
+                string destinationPath = Path.Combine(path, "Chronological", i.ToString().PadLeft(3, '0') + " " + result.chapter + " " + chapters);
+                Directory.CreateDirectory(destinationPath);
+                foreach (string mp3 in result.mp3S)
+                {
+                    File.Copy(mp3, Path.Combine(destinationPath, Path.GetFileName(mp3)), true);
+                }
+                i++;
+            }
+        }
 
 		private void Button_Click(object sender, RoutedEventArgs e)
 		{
-			Refresh();
+            CopyMp3s();
 		}
 	}
 }
--- Chronological/Chronological/MatchingLineToMp3.cs
+++ Chronological/Chronological/MatchingLineToMp3.cs
@@ -5,20 +5,26 @@ namespace Chronological
 {
 	public class MatchingLineToMp3
 	{
-		string line;
-		List<string> mp3S;
-		string result;
+		public string line;
+        public string chapter;
+        public int start;
+        public int end;
+		public List<string> mp3S;
+		public string result;
 
-		public MatchingLineToMp3(string line, List<string> mp3S, string result)
+		public MatchingLineToMp3(string line, string chapter, int start, int end, List<string> mp3S, string result)
 		{
 			this.line = line;
+            this.chapter = chapter;
+            this.start = start;
+            this.end = end;
 			this.mp3S = mp3S;
 			this.result = result;
 		}
 
 		public override string ToString()
 		{
-			return string.Format("{0} - {1} => {2} mp3s", result, line, mp3S.Count);
+			return string.Format("{0} - {1} => {2} mp3s", result, line, (mp3S != null) ? mp3S.Count : 0);
 		}
 	}
 }
